65b0518fe821ac55c9f61716e01185005e6b24ec
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-02-19 01:18:09 +0800

    refactor: upgrade jvmTarget and gradle
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,8 +43,8 @@ fun Project.configureBaseExtension() {
         }
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
             isCoreLibraryDesugaringEnabled = true
         }
 
