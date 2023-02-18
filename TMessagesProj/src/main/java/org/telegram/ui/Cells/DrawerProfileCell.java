90c939e95363775fcf0ab3b364c5c8714f6993ff
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-02-19 03:26:25 +0800

    feat: truly hide phone in drawer
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/DrawerProfileCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/DrawerProfileCell.java
@@ -756,7 +756,8 @@ public class DrawerProfileCell extends FrameLayout implements NotificationCenter
         } else if (!TextUtils.isEmpty(user.username)) {
             phoneTextView.setText("@" + user.username);
         } else {
-            phoneTextView.setText(LocaleController.getString("MobileHidden", R.string.MobileHidden));
+//            phoneTextView.setText(LocaleController.getString("MobileHidden", R.string.MobileHidden));
+            phoneTextView.setVisibility(GONE);
         }
         AvatarDrawable avatarDrawable = new AvatarDrawable(user);
         avatarDrawable.setColor(Theme.getColor(Theme.key_avatar_backgroundInProfileBlue));
