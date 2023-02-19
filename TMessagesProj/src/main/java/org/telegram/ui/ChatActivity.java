af51a40306ea09d1047c80ecbb5499f497cebbb9
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-02-20 00:56:34 +0800

    feat: hide message seen tooltip
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -25759,9 +25759,11 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                             }
 
                             if (SharedConfig.messageSeenHintCount > 0 && contentView.getKeyboardHeight() < AndroidUtilities.dp(20)) {
-                                messageSeenPrivacyBulletin = BulletinFactory.of(Bulletin.BulletinWindow.make(getContext()), themeDelegate).createErrorBulletin(AndroidUtilities.replaceTags(LocaleController.getString("MessageSeenTooltipMessage", R.string.MessageSeenTooltipMessage)));
-                                messageSeenPrivacyBulletin.setDuration(4000);
-                                messageSeenPrivacyBulletin.show();
+                                if (Config.hideMessageSeenTooltip) {
+                                    messageSeenPrivacyBulletin = BulletinFactory.of(Bulletin.BulletinWindow.make(getContext()), themeDelegate).createErrorBulletin(AndroidUtilities.replaceTags(LocaleController.getString("MessageSeenTooltipMessage", R.string.MessageSeenTooltipMessage)));
+                                    messageSeenPrivacyBulletin.setDuration(4000);
+                                    messageSeenPrivacyBulletin.show();
+                                }
                                 SharedConfig.updateMessageSeenHintCount(SharedConfig.messageSeenHintCount - 1);
                             }
 
