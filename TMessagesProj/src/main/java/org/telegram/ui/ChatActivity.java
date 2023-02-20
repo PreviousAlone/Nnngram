fb61bd385676f70955198c642e40b51054b3d096
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-02-21 01:46:09 +0800

    feat: show online status
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -5999,7 +5999,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                         if (updateVisibility) {
                             imageReceiver.setVisible(true, false);
                         }
-                        imageReceiver.draw(canvas);
+                        cell.drawStatusWithImage(canvas, imageReceiver, AndroidUtilities.dp(7));
                         canvas.restore();
 
                         if (!replaceAnimation && child.getTranslationY() != 0) {
