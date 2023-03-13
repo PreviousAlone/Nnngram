555767d91bc83a6c068e0dfc027cfdbd3a2b374d
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-03-14 01:30:41 +0800

    feat: add toast when video can't play
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/ui/Components/VideoPlayer.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/VideoPlayer.java
@@ -22,6 +22,7 @@ import android.view.Surface;
 import android.view.SurfaceView;
 import android.view.TextureView;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -662,6 +663,7 @@ public class VideoPlayer implements Player.Listener, VideoListener, AnalyticsLis
                     }
                 }
             } else {
+                Toast.makeText(ApplicationLoader.applicationContext, error.getErrorCodeName(), Toast.LENGTH_LONG).show();
                 delegate.onError(this, error);
             }
         });
