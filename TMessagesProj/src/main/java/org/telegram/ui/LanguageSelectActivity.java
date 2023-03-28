icewithcola/Kaguram

refactor: make it nagram, thou nnngram now
Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

TMessagesProj/src/main/java/org/telegram/ui/LanguageSelectActivity.java
@@ -58,7 +58,8 @@ import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Timer;
 
-import top.qwq2333.nullgram.activity.GeneralSettingActivity;
+import xyz.nextalone.nnngram.activity.GeneralSettingActivity;
+
 
 public class LanguageSelectActivity extends BaseFragment implements NotificationCenter.NotificationCenterDelegate {
 
@@ -641,7 +642,7 @@ public class LanguageSelectActivity extends BaseFragment implements Notification
                 case VIEW_TYPE_SETTINGS: {
                     TextSettingsCell settingsCell = (TextSettingsCell) holder.itemView;
                     settingsCell.updateRTL();
-                    settingsCell.setText(LocaleController.getString("NullSettings", R.string.NullSettings), false);
+                    settingsCell.setText(LocaleController.getString("NnngramName", R.string.NullgramName), false);
                     /*HashSet<String> langCodes = RestrictedLanguagesSelectActivity.getRestrictedLanguages();
                     final String doNotTranslateCellName = LocaleController.getString("DoNotTranslate", R.string.DoNotTranslate);
                     String doNotTranslateCellValue = null;

PreviousAlone/Nnngram

TMessagesProj/src/main/java/org/telegram/ui/LanguageSelectActivity.java
@@ -59,7 +59,8 @@ import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Timer;
 
-import top.qwq2333.nullgram.activity.GeneralSettingActivity;
+import xyz.nextalone.nnngram.activity.GeneralSettingActivity;
+
 
 public class LanguageSelectActivity extends BaseFragment implements NotificationCenter.NotificationCenterDelegate {
 
@@ -643,7 +644,7 @@ public class LanguageSelectActivity extends BaseFragment implements Notification
                 case VIEW_TYPE_SETTINGS: {
                     TextSettingsCell settingsCell = (TextSettingsCell) holder.itemView;
                     settingsCell.updateRTL();
-                    settingsCell.setText(LocaleController.getString("NullSettings", R.string.NullSettings), false);
+                    settingsCell.setText(LocaleController.getString("NnngramName", R.string.NnngramName), false);
                     /*HashSet<String> langCodes = RestrictedLanguagesSelectActivity.getRestrictedLanguages();
                     final String doNotTranslateCellName = LocaleController.getString("DoNotTranslate", R.string.DoNotTranslate);
                     String doNotTranslateCellValue = null;