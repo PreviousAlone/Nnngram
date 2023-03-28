27efb1f544bdbf08655b5f3c4c8d92e3b7526f89
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-03-28 22:41:23 +0800

    feat: update sticker desc
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/xyz/nextalone/nnngram/ui/StickerSizePreviewMessagesCell.java
+++ TMessagesProj/src/main/java/xyz/nextalone/nnngram/ui/StickerSizePreviewMessagesCell.java
@@ -90,7 +90,7 @@ public class StickerSizePreviewMessagesCell extends LinearLayout {
         message.out = false;
         message.peer_id = new TLRPC.TL_peerUser();
         message.peer_id.user_id = 1;
-        messageObjects[0].customReplyName = "James Clef";
+        messageObjects[0].customReplyName = "NextAlone";
         messageObjects[0].replyMessageObject = new MessageObject(UserConfig.selectedAccount, message, true, false);
 
         message = new TLRPC.TL_message();
