981277fe11353355b7cea9dbf840e47635c190aa
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-04-04 21:09:14 +0800

    feat: change quick msg icon
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -29101,7 +29101,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                             if (allowChatActions) {
                                 items.add(ConfigManager.getStringOrDefault(Defines.customQuickMessageDisplayName, "NULL"));
                                 options.add(Defines.customQuickMessageRow);
-                                icons.add(R.drawable.msg_fave);
+                                icons.add(R.drawable.msg_prpr);
                             }
                         }
                         if (Config.showViewHistory) {
