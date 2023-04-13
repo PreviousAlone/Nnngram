0430289f63d141b73de71831cf75e2a70d22acd6
‭huajijam <strhuaji@gmail.com>
2023-04-13 22:26:38 +0800

    feat: optimize hidePhone

--- TMessagesProj/src/main/java/org/telegram/ui/ProfileActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ProfileActivity.java
@@ -10471,10 +10471,16 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
                     } else if (position == numberRow) {
                         TLRPC.User user = UserConfig.getInstance(currentAccount).getCurrentUser();
                         String value;
-                        if (user != null && user.phone != null && user.phone.length() != 0) {
-                            value = PhoneFormat.getInstance().format("+" + user.phone);
+                        if (!Config.hidePhone) {
+                            if (user != null && user.phone != null && !user.phone.isEmpty()) {
+                                value = PhoneFormat.getInstance().format("+" + user.phone);
+                            } else {
+                                value = LocaleController.getString("NumberUnknown", R.string.NumberUnknown);
+                            }
+                        } else if (!TextUtils.isEmpty(user.username)) {
+                            value = "@" + user.username;
                         } else {
-                            value = LocaleController.getString("NumberUnknown", R.string.NumberUnknown);
+                            value = "@???";
                         }
                         detailCell.setTextAndValue(value, LocaleController.getString("TapToChangePhone", R.string.TapToChangePhone), true);
                         detailCell.setContentDescriptionValueFirst(false);
