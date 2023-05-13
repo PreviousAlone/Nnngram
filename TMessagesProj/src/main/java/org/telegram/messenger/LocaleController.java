7667d15c087511f6e654d58079c647d96efe01f9
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-05-13 15:36:40 +0800

    revert: telegram replace
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/messenger/LocaleController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/LocaleController.java
@@ -1149,8 +1149,8 @@ public class LocaleController {
             value = "LOC_ERR:" + key;
         }
 
-        if (value.contains("Telegram"))
-            value = value.replace("Telegram", "Nnngram");
+//        if (value.contains("Telegram"))
+//            value = value.replace("Telegram", "Nnngram");
         if (value.contains("TELEOFFICIAL"))
             value = value.replace("TELEOFFICIAL", "Telegram");
         if (value.contains("Nullgram"))
@@ -1303,11 +1303,14 @@ public class LocaleController {
                 }
             }
 
-            if (value.contains("Telegram")) {
-                value = value.replace("Telegram", "Nullgram");
-            }
+//            if (value.contains("Telegram")) {
+//                value = value.replace("Telegram", "Nullgram");
+//            }
             if (value.contains("TELEOFFICIAL")) {
                 value = value.replace("TELEOFFICIAL", "Telegram");
+            }
+            if (value.contains("Nullgram")) {
+                value = value.replace("Nullgram", "Nnngram");
             }
 
             if (getInstance().currentLocale != null) {
