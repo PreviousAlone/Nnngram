62a6cc1c360e3363e0aaaffbac615255620a36be
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-09-14 18:23:36 +0800

    fix: topic icon lost
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/DialogCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/DialogCell.java
@@ -1915,7 +1915,8 @@ public class DialogCell extends BaseCell implements StoriesListPlaceProvider.Ava
                 }
             }
         }
-        nameString = StringUtils.zalgoFilter(nameString);
+        if (topicIconInName == null)
+            nameString = StringUtils.zalgoFilter(nameString);
         int timeWidth;
         if (drawTime) {
             timeWidth = (int) Math.ceil(Theme.dialogs_timePaint.measureText(timeString));
