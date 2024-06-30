347f3c61f0d9defa180e11c8393732667dd9bb5f
Next Alone <12210746+NextAlone@users.noreply.github.com>
2024-07-01 02:59:20 +0800

    fix: when disable auto pip, video still playing
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
+++ TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
@@ -8403,8 +8403,11 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
         } else {
             pipVideoOverlayAnimateFlag = false;
 
-            if (!Config.disableAutoPip)
+            if (!Config.disableAutoPip) {
                 switchToPip(false);
+            } else {
+                videoPlayer.pause();
+            }
         }
     }
 
