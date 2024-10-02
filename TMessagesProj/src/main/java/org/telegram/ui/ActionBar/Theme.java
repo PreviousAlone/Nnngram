083b031850eaf8030a2cbcb7272e2903a7ed2e39
Next Alone <12210746+NextAlone@users.noreply.github.com>
2024-10-03 05:00:23 +0800

    fix: custom theme not follow system-based dark mode toggle
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/ui/ActionBar/Theme.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ActionBar/Theme.java
@@ -2523,7 +2523,8 @@ public class Theme {
         }
 
         public boolean isLight() {
-            return pathToFile == null && !isDark();
+//            return pathToFile == null && !isDark();  // don't use pathToFile to determine light theme
+            return !isDark();
         }
 
         public String getKey() {
