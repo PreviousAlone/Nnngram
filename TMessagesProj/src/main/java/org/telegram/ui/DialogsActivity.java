5a73db68550f1b3198944092268fd9d4d8c21e91
Next Alone <12210746+NextAlone@users.noreply.github.com>
2024-10-07 22:39:44 +0800

    fix: can't hide archive after opening topics
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -5416,7 +5416,7 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
                 if (!hasStories) {
                     DialogsActivity.this.setScrollY(0);
                 }
-                if (!hasFragment()) {
+                if (!hasFragment() && hasStories) {
                     invalidateScrollY = true;
                     fixScrollYAfterArchiveOpened = true;
                     fragmentView.invalidate();
