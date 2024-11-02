83d86dcf372c0265e6fdb2a6f52cc4a53089338a
Next Alone <12210746+NextAlone@users.noreply.github.com>
2024-11-02 22:57:04 +0800

    feat: make proxy button invisible instead of hidded
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -10039,7 +10039,7 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
             return;
         }
         if (Config.hideProxyEntryInTitle) {
-            proxyItem.setVisibility(View.GONE);
+            proxyItem.setVisibility(View.INVISIBLE);
             return;
         }
         boolean showDownloads = false;
