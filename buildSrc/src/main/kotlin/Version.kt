5d3ac9b7614f8191bf6ef2a568f995f400b483af
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-01-15 15:58:51 +0800

    build: fix local build

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -18,32 +18,13 @@
  */
 
 import java.io.File
-import java.util.Properties
 import org.gradle.api.JavaVersion
 
 object Version {
     fun findBuildToolsVersion(): String {
-        val defaultBuildToolsVersion = "33.0.0"
+        val defaultBuildToolsVersion = "35.0.1"
         return File(System.getenv("ANDROID_HOME"), "build-tools").listFiles()?.filter { it.isDirectory }?.maxOfOrNull { it.name }?.also { println("Using build tools version $it") }
             ?: defaultBuildToolsVersion
-    }
-
-    fun findNdkVersion(): String {
-        val defaultNdkVersion = "23.2.8568313"
-        val ndkBundle = File(System.getenv("ANDROID_HOME"), "ndk-bundle")
-        if (ndkBundle.exists()) {
-            val sourceProperties = File(ndkBundle, "source.properties")
-            if (sourceProperties.exists()) {
-                val properties = Properties()
-                sourceProperties.inputStream().use {
-                    properties.load(it)
-                }
-                return properties.getProperty("Pkg.Revision").also { if (it != null) println("Using ndk version $it") }
-                    ?: defaultNdkVersion
-            }
-        }
-        return File(System.getenv("ANDROID_HOME"), "ndk").listFiles()?.filter { it.isDirectory }?.maxOfOrNull { it.name }.also { if (it != null) println("Using ndk version $it") }
-            ?: defaultNdkVersion
     }
 
     @JvmStatic
