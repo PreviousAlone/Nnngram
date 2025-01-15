d783bf9e4a13d46aeee749d3ad0af079e163aecb
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-01-15 15:58:51 +0800

    build: fix local build

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ val androidTargetSdkVersion by extra(35)
 val androidMinSdkVersion by extra(27)
 val androidCompileSdkVersion by extra(35)
 val androidBuildToolsVersion = Version.findBuildToolsVersion()
-val androidCompileNdkVersion = "27.0.12077973"
+val androidCompileNdkVersion = "27.2.12479018"
 
 fun Project.configureBaseExtension() {
     extensions.findByType(com.android.build.gradle.BaseExtension::class)?.run {
