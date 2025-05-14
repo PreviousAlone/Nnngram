bc8a906015630c5fcea3a752335e82f652bcb7a4
xtaodada <xtao@xtao.de>
2025-05-14 21:36:27 +0800

    feat: always show edit cover button when send video

--- TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
+++ TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
@@ -14672,7 +14672,7 @@ accountInstance.getUserConfig().getClientUserId(), false, false, true, 0, 0);
                                 mirrorItem.setVisibility(View.GONE);
                                 mirrorItem.setTag(null);
                                 AndroidUtilities.updateViewVisibilityAnimated(muteButton, true, 1f, animated);
-                                AndroidUtilities.updateViewVisibilityAnimated(editCoverButton, parentChatActivity != null && (UserObject.isUserSelf(parentChatActivity.getCurrentUser()) || ChatObject.isChannelAndNotMegaGroup(parentChatActivity.getCurrentChat()) && (duration >= 20 || coverPhoto != null || coverPath != null)), 1f, animated);
+                                AndroidUtilities.updateViewVisibilityAnimated(editCoverButton, true, 1f, animated);
                                 if (coverPhoto != null) {
                                     editCoverButton.setImage(coverPhoto, coverPhotoObject);
                                 } else {
