aab47e47ea7fc4865aff5928ef6707c529bdc9ef
Next Alone <12210746+NextAlone@users.noreply.github.com>
2025-06-08 22:55:08 +0800

    fix: hide sending stories button when disabled
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -8798,13 +8798,13 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
             floatingButton.setAnimation(R.raw.write_contacts_fab_icon, 52, 52);
             floatingButtonContainer.setContentDescription(LocaleController.getString(R.string.NewMessageTitle));
 
-            if (floatingButton2 != null) {
-                floatingButton2.setImageResource(R.drawable.filled_premium_camera);
-                floatingButton2Container.setContentDescription(LocaleController.getString(R.string.AccDescrCaptureStory));
-                if (floatingButtonContainer.getVisibility() == View.VISIBLE) {
-                    floatingButton2Container.setVisibility(View.VISIBLE);
-                }
-            }
+//            if (floatingButton2 != null) {
+//                floatingButton2.setImageResource(R.drawable.filled_premium_camera);
+//                floatingButton2Container.setContentDescription(LocaleController.getString(R.string.AccDescrCaptureStory));
+//                if (floatingButtonContainer.getVisibility() == View.VISIBLE) {
+//                    floatingButton2Container.setVisibility(View.VISIBLE);
+//                }
+//            }
         }
     }
 
@@ -13305,7 +13305,7 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
     }
 
     private void openStoriesRecorder() {
-        if (!storiesEnabled) {
+        if (!storiesEnabled && !Config.hideStories) {
             if (storyPremiumHint != null) {
                 if (storyPremiumHint.shown()) {
                     return;
